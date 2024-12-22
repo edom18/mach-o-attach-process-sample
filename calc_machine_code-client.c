cfc8f334acf8ac184a3b459ccc4785796e3dc602
edom18 <edo.m18@gmail.com>
2024-12-22 14:45:00 +0900

    chore: updated a client.

--- calc_machine_code-client.c
+++ calc_machine_code-client.c
@@ -9,16 +9,28 @@ int main()
     unsigned char shell_code[4 * 4];
     calculate_machine_code(address, 0, shell_code);
 
+    // 4 命令分を表示
     for (int i = 0; i < 4; i++)
     {
         unsigned long result = 0;
         for (int j = 0; j < 4; j++)
         {
             int index = (i * 4) + j;
-            result += ((unsigned long)shell_code[index] << (8 * (3 - j)));
+            unsigned long tmp = (unsigned long)shell_code[index] << (8 * j);
+            result += tmp;
         }
         printf("%d: %lx -- ", i, result);
         print_binary(result);
+    }
+
+    for (int i = 0; i < 16; i++)
+    {
+        if (i % 4 == 0)
+        {
+            printf("\n");
+        }
+
+        printf("0x%x ", shell_code[i]);
     }
 
     return 0;
