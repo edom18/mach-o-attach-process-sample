24611569c82fcc796eaffa162943640668487e0f
edom18 <edo.m18@gmail.com>
2024-12-22 10:24:02 +0900

    add: Added a client for checking calc_machine_code function.

---
+++ calc_machine_code-client.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+extern void print_binary(unsigned long value);
+extern void calculate_machine_code(uintptr_t value, unsigned int register_number, unsigned char* machine_code_array);
+
+int main()
+{
+    uintptr_t address = 0x180451c04;
+    unsigned char shell_code[32];
+    calculate_machine_code(address, 0, shell_code);
+
+    for (int i = 0; i < 4; i++)
+    {
+        unsigned long result = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            int index = (i * 4) + j;
+            result += ((unsigned long)shell_code[index] << (8 * (3 - j)));
+        }
+        printf("%d: %lx -- ", i, result);
+        print_binary(result);
+    }
+
+    return 0;
+}
