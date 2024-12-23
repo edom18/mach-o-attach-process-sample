72d0f7c18d4828e5bf8606f533cc8485a6b78555
edom18 <edo.m18@gmail.com>
2024-12-23 22:28:16 +0900

    feat: Added some commands to check injected library things.

--- wait-input.c
+++ wait-input.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <mach-o/dyld.h>
 
 #define BUFFER_SIZE 100
 
@@ -9,6 +12,13 @@ int main()
     char input[BUFFER_SIZE];
 
     printf("Please input any text. Exit to type input 'exit'\n");
+
+    printf("----\n");
+
+    pid_t pid = getpid();
+    printf("pid is [%d]\n", pid);
+
+    // void* libsample = dlopen("./libsample.dylib", 2);
 
     while (1)
     {
@@ -21,6 +31,32 @@ int main()
             {
                 printf("Exiting.\n");
                 break;
+            }
+            else if (strcmp(input, "list") == 0)
+            {
+                printf("Listing symbols...\n");
+
+                uint32_t image_count = _dyld_image_count();
+                printf("Dynamic image count: %d\n", image_count);
+
+                for (uint32_t i = 0; i < image_count; i++)
+                {
+                    const char* name = _dyld_get_image_name(i);
+                    printf("Lib name: %s\n", name);
+                }
+            }
+            else if (strcmp(input, "pid") == 0)
+            {
+                void (*print_pid_fn)() = dlsym(RTLD_DEFAULT, "print_pid");
+                if (print_pid_fn)
+                {
+                    printf("Printing PID...\n");
+                    print_pid_fn();
+                }
+                else
+                {
+                    printf("print_pid function is not found.\n");
+                }
             }
             else
             {
